06cb1af4cc4dea3a33930ddfb2f0b2decd119c75
Angelo Rodrigues <angelorodrigues318@gmail.com>
2023-01-20 21:06:14 -0300

    Create Table and install cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,22 @@
 //Metodo HTTP: Get(busca) Post(criar) Put(atualizar) Patch(atualizar info especifica) Delete
-
 import fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.get('/', () => {
-    return 'Hello world'
+app.register(cors);
+
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany({
+
+    })
+    return habits
 })
 
 app.listen({
-    port: 3333
+    port: 3333,
 }).then(() => {
     console.log('HTTP Server listening on port 3333')
 });
